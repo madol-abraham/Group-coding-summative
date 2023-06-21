af213981dc0506086481a77ca08cd86c1aab383c
MyUser <f.akinwande@alustudent.com>
2023-06-21 03:22:01 -0700

    move to directory

--- negpod_id-q1/Move-to-directory.sh
+++ negpod_id-q1/Move-to-directory.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+##!/usr/bin/env bash
+# This scripts automattically moves all four files
 
-mkdir 0323-RW-SOFEN-8120120-2_q1
-mv main.sh students-list_0333.txt select-emails.sh student-emails.txt 0323-RW-SOFEN-8120120-2_q1/
+mv main.sh students-list_0333.txt select-emails.sh student-emails.txt negpod_id-q1/
