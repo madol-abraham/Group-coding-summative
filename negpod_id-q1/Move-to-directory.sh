2426e85a4d53ea6d4f8685102f9315d43a4c414c
madol-abraham <m.madol@alustudent.com>
2023-06-24 11:35:40 -0700

    updated

--- negpod_id-q1/Move-to-directory.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mkdir 0323-RW-SOFEN-8120120-2_q1
-mv main.sh students-list_0333.txt select-emails.sh student-emails.txt 0323-RW-SOFEN-8120120-2_q1/
--- negpod_id-q1/backup-0323-RW-SOFEN-8120120-1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-HOST="a0dfc11ba139.a982e858.alu-cod.online"
-USER="a0dfc11ba139"
-PASSWORD="899c79fc532cd0591af1"
-SOURCE="0323-RW-SOFEN-8120120-2_q1"
-DESTINATION="/home/sftp-student/03033/summative"
-
-sshpass -p $PASSWORD scp -r $SOURCE $USER@$HOST:$DESTINATION
