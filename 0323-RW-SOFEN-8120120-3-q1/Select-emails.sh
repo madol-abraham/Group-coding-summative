87bcbc6c40a80b07694f99081ff627faca7a7ac1
EUNICE ADEWUSI (CLIMIRADI) <e.adewusi@alustudent.com>
2023-06-24 22:59:08 +0300

    Update Select-emails.sh

--- 0323-RW-SOFEN-8120120-3-q1/Select-emails.sh
+++ 0323-RW-SOFEN-8120120-3-q1/Select-emails.sh
@@ -1,4 +1,5 @@
 #/usr/bin/bash
+#This script creates a command to select just the emails of the student and saves the output in “student-emails.txt” file.
 
 students_file="students-list_0333.txt"
 output_file="student-emails.txt"
