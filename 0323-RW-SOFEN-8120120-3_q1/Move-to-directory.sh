23034b31e94e999b733435451636687d6fd6152f
EUNICE ADEWUSI (CLIMIRADI) <e.adewusi@alustudent.com>
2023-06-24 21:50:55 +0300

    Delete 0323-RW-SOFEN-8120120-3_q1 directory

--- 0323-RW-SOFEN-8120120-3_q1/Move-to-directory.sh
+++
@@ -1,4 +0,0 @@
-##!/usr/bin/env bash
-# This scripts automattically moves all four files
-
-mv main.sh students-list_0333.txt select-emails.sh student-emails.txt negpod_id-q1/
--- 0323-RW-SOFEN-8120120-3_q1/backup-0323-RW-SOFEN-8120120-3.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# This script backup the directory created 
-
-HOST="a0dfc11ba139.a982e858.alu-cod.online"
-USERNAME="a0dfc11ba139"
-PASSWORD="899c79fc532cd0591af1"
-Location=:/home/sftp-student/03033/summative
-
-sshpass -p $PASSWORD scp -r $SOURCE $USER@$HOST:$DESTINATION
